b04c60b231e5fd08f490acd8d95bb966bca1140a
성신 <seongsin@seongsins-MacBook-Air.local>
2026-01-07 14:34:33 +0900

    generic problem

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+fn find<T:PartialEq>(n:T,array:&Vec<T>) -> bool{
+    for s in array{
+        if *s == n{
+            return true;
+        }
+    }
+    return false;
+    
+}
+
 fn main() {
-    println!("Hello, world!");
+    let number = vec![1,2,3,4,5,6,7,8,9,10];
+    println!("{}",find(1,&number));
+    println!("{}",find(11,&number));
 }
