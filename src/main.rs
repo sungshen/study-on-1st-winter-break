1ae92242d1a15f2a80f815dfe6d1513d924b2d85
성신 <seongsin@201-40-21-100.bsaco300.ipd.brasiltelecom.net.br>
2026-01-08 16:59:35 +0900

    last

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,8 @@
-trait Shape {
-    fn area(&self) -> i32;
+struct SelfRef {
+    data: String,
+    ptr: *const String,  // data를 가리킴
 }
 
-struct Rect {
-    width: i32,
-    height: i32,
+fn main() {
 }
 
-struct Circle {
-    r: i32,
-}
-
-impl Shape for Rect {
-    fn area(&self) -> i32 {
-        self.width * self.height
-    }
-}
-
-impl Shape for Circle {
-    fn area(&self) -> i32 {
-        self.r * self.r * 3
-    }
-}
-
-fn print_area<T: Shape>(x: &T) {
-    println!("{}", x.area());
-}
-
-fn print_area_dyn(x: &dyn Shape) {
-    println!("{}", x.area());
-}
-
-fn main(){
-    let r = Rect { width: 3, height: 4 };
-    let c = Circle { r: 5 };
-
-    print_area(&r);
-    print_area(&c);
-}
